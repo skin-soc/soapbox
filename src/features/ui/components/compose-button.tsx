c2ddeb93ef83a34336377bbc64338ea39269ef0f
skin-soc <82213990+skin-soc@users.noreply.github.com>
2026-03-11 22:36:18 +0100

    custom locale en

--- src/features/ui/components/compose-button.tsx
+++ src/features/ui/components/compose-button.tsx
@@ -1,4 +1,5 @@
-import { FormattedMessage } from 'react-intl';
+import { useMemo } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { useLocation, useRouteMatch } from 'react-router-dom';
 
 import { groupComposeModal } from '@/actions/compose.ts';
@@ -23,7 +24,7 @@ const ComposeButton = () => {
   return <HomeComposeButton />;
 };
 
-const HomeComposeButton = () => {
+/* const HomeComposeButton = () => {
   const dispatch = useAppDispatch();
   const onOpenCompose = () => dispatch(openModal('COMPOSE'));
 
@@ -34,7 +35,39 @@ const HomeComposeButton = () => {
       onClick={onOpenCompose}
       block
     >
-      <FormattedMessage id='navigation.compose' defaultMessage='Compose' />
+      <FormattedMessage id='navigation.compose' defaultMessage='Stomp!' />
+    </Button>
+  );
+}; */
+
+const HomeComposeButton = () => {
+  const dispatch = useAppDispatch();
+  const intl = useIntl();
+
+  const onOpenCompose = () => dispatch(openModal('COMPOSE'));
+
+  // Get the array of stomp variants from the locale file
+  const stompVariants = useMemo(() => {
+    const variants = intl.messages['compose.stomp_variants'];
+    return Array.isArray(variants) && variants.length > 0
+      ? variants
+      : ['Stomp!']; // fallback if array is missing or empty
+  }, [intl.messages]);
+
+  // Pick one random variant (only re-rolls when locale changes or component remounts)
+  const randomStomp = useMemo(() => {
+    const idx = Math.floor(Math.random() * stompVariants.length);
+    return stompVariants[idx] as string;
+  }, [stompVariants]);
+
+  return (
+    <Button
+      theme='accent'
+      size='lg'
+      onClick={onOpenCompose}
+      block
+    >
+      {randomStomp}
     </Button>
   );
 };
@@ -60,7 +93,7 @@ const GroupComposeButton = () => {
       <HStack space={3} alignItems='center'>
         <Avatar className='-my-1 border-2 border-white' size={30} src={group.avatar} />
         <span>
-          <FormattedMessage id='navigation.compose_group' defaultMessage='Compose to Group' />
+          <FormattedMessage id='navigation.compose_group' defaultMessage='Stomp in Group' />
         </span>
       </HStack>
     </Button>
